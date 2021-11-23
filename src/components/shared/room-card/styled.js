68eb8d7d08b1b378920aecebc49018707cd158b0
전효원 <hyowon612@gmail.com>
2021-11-23 21:19:18 +0900

    [#6] 유저카드 UI 적용

--- src/components/shared/room-card/styled.js
+++ src/components/shared/room-card/styled.js
@@ -1,37 +1,58 @@
 import styled from "styled-components";
 
 export const Card = styled.button`
-  width: 20vw;
-  height: 4vw;
+  width: 22vw;
+  height: 5vw;
   border: none;
   border-radius: 5px;
-  margin-left: 0.5vw;
   margin-top: 1vw;
-  font-size: 14px;
+  font-size: 16px;
   background-color: white;
   cursor: pointer;
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
 `;
 export const Left = styled.div`
-  display: block;
-  margin-left: 1vw;
-  width: 5vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 18vw;
+  height: 4vw;
   text-align: left;
 `;
 export const Right = styled.div`
-  display: block;
-  text-align: left;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-between;
+  height: 4vw;
 `;
 
 export const Name = styled.div`
-  width: 5vw;
+  font-size: 24px;
+  font-weight: bold;
   text-overflow: ellipsis;
 `;
 export const Text = styled.div`
+  height: 18px;
   overflow: hidden;
+  white-space: nowrap;
   text-overflow: ellipsis;
 `;
-export const Count = styled.div``;
-export const Waiting = styled.div``;
+export const Count = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  float: right;
+  width: 2vw;
+  height: 2vw;
+  border-radius: 1vw;
+  font-weight: bold;
+  background-color: gray;
+  color: white;
+`;
+export const Waiting = styled.div`
+  font-weight: bold;
+  color: red;
+`;
