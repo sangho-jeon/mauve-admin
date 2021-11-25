5550717b768e3ae23a8784bc7f5a2b2c69845639
전효원 <hyowon612@gmail.com>
2021-11-25 16:37:30 +0900

    [#19] 유저카드 레이아웃 변경

--- src/components/shared/room-card/index.jsx
+++ src/components/shared/room-card/index.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { Card, Name, Text, Count, Waiting, Left, Right } from "./styled";
+import { Card, Name, Text, Count, Waiting, Left, Right, Top, Bottom } from "./styled";
 
 const RoomCard = (props) => {
   return (
     <Card onClick={props.click} value={props.id}>
-      <Left value={props.id}>
-        <Name>{props.name}</Name>
-        <Text>{props.text}</Text>
-        {props.id}
-      </Left>
-      <Right>
-        <Count>{props.count}</Count>
-        <Waiting>{props.wait}</Waiting>
-      </Right>
+      <Top>
+        <Left>
+          <Name>{props.name}</Name>
+          <Count>{props.count}</Count>
+        </Left>
+        <Right>
+        </Right>
+      </Top>
+      <Bottom>
+        <Text>텍스트텍스트스트텍스트스트텍스트트텍스트스트텍스트트텍스트텍스트{props.text}</Text>
+        <Waiting>13:38{props.wait}</Waiting>
+      </Bottom>
     </Card>
   );
 };
