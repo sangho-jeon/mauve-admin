9b1df6d15b41bd9eb0817aa9a83235a9c4e19dc9
전효원 <hyowon612@gmail.com>
2021-11-26 20:32:56 +0900

    [#27] 노트테이킹 useState 추가

--- src/components/feature/chatblock/info/note/styled.js
+++ src/components/feature/chatblock/info/note/styled.js
@@ -1,8 +1,24 @@
 import styled from "styled-components";
 
 export const Note = styled.div`
+  box-sizing: border-box;
   width: 38vw;
   height: 15vw;
+  padding: 1vw;
   background-color: white;
   box-shadow: 0px 5px 10px #00000015;
+`;
+
+export const Title = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const NoteInput = styled.textarea`
+  width: 100%;
+  height: 11vw;
+  margin-top: 0.5vw;
+  resize: none;
+  border-style: none;
+  font-size: 14px;
 `;
