854fbe6cbcfa0fe150f93b4d034cefe52673d211
전효원 <hyowon612@gmail.com>
2021-11-29 15:54:38 +0900

    [#30] 회원 기본정보 레이아웃 추가

--- src/components/feature/chatblock/info/profile/index.jsx
+++ src/components/feature/chatblock/info/profile/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Profile } from "./styled";
+import { Profile, Information, Item, Tag } from "./styled";
+import Calendar from "./calendar/calendar";
 
 const ProfileSection = () => {
-  return (
-  <Profile>프로필</Profile>
-  );
+	return (
+		<Profile>
+		<Information>
+		<Item><Tag>이름:</Tag></Item>
+		<Item><Tag>번호:</Tag></Item>
+		<Item><Tag>나이:</Tag></Item>
+		<Item><Tag>신장:</Tag></Item>
+		<Item><Tag>체중:</Tag></Item>
+		<Item><Tag>결제 남은 일수:</Tag></Item>
+		</Information>
+		<Calendar />
+		</Profile>
+	);
 };
 
 export default ProfileSection;
--- src/components/feature/chatblock/info/profile/styled.js
+++ src/components/feature/chatblock/info/profile/styled.js
@@ -1,8 +1,30 @@
 import styled from "styled-components";
 
 export const Profile = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 38vw;
   height: 15vw;
   background-color: white;
   box-shadow: 0px 5px 10px #00000015;
+`;
+
+export const Information = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 16vw;
+  height: 15vw;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5vw;
+`;
+
+export const Tag = styled.div`
+  margin-right: 0.5vw;
+  font-size: 18px;
+  font-weight: bold;
 `;
