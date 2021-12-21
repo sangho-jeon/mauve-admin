ebbd96c769452b4ea2d44c59050400de4a04f872
sangho-jeon <jeonsangho1587@gmail.com>
2021-12-21 09:13:01 +0900

    fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import LoginPage from "./pages/login";
 import MainPage from "./pages/main";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginPage /> */}
-      <MainPage />
+      <LoginPage />
+      {/* <MainPage /> */}
     </div>
   );
 }
---
+++ src/components/login/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { LoginBlock } from "./styled";
+
+const Login = () => {
+  return <LoginBlock>login paekkkkx</LoginBlock>;
+};
+
+export default Login;
