d2872d345d70f690af702374683516b260322f51
Hyowon Jeon <hyowon612@gmail.com>
2021-12-22 21:01:07 +0900

    [#42] 주기 더미데이터 적용

--- src/components/feature/chatblock/info/profile/calendar/calendar.jsx
+++ src/components/feature/chatblock/info/profile/calendar/calendar.jsx
@@ -2,9 +2,22 @@ import React from "react";
 import './calendar.css';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import dummy from './dummy.json';
+import dummy from '../dummy.json';
 
 const Calendar = () => {
+
+	const getStart = (e) => {
+		if (e.phase === "period") {
+			return e.start_date;
+		}
+	};
+
+	const getEnd = (e) => {
+		if (e.phase === "period") {
+			return e.end_date;
+		}
+	};
+
 	return (
 		<FullCalendar
 		plugins={[ dayGridPlugin ]}
@@ -16,11 +29,11 @@ const Calendar = () => {
 			end: "",
 		}}          
 		events = {
-      dummy.map((dummy) => (
+      dummy.body.periodPhaseRecord.map((dummy) => (
         {
           allDay: true,
-          start: new Date(dummy.start),
-          end: new Date(dummy.end).setDate(new Date(dummy.end).getDate() + 1),
+          start: new Date(getStart(dummy)),
+          end: new Date(getEnd(dummy)).setDate(new Date(getEnd(dummy)).getDate() + 1),
         }
       ))
 		}
