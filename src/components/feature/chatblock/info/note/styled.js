816aa2e264dd92b5614c80e10037bb706c01fee1
Hyowon Jeon <hyowon612@gmail.com>
2021-12-28 18:33:28 +0900

    [#39] 전체 레이아웃 조정

--- src/components/feature/chatblock/info/note/styled.js
+++ src/components/feature/chatblock/info/note/styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Note = styled.div`
   box-sizing: border-box;
-  width: 38vw;
-  height: 15vw;
+  width: 41vw;
+  height: 17vw;
   padding: 1vw;
   background-color: white;
   box-shadow: 0px 5px 10px #00000015;
@@ -16,7 +16,7 @@ export const Title = styled.div`
 
 export const NoteInput = styled.textarea`
   width: 100%;
-  height: 11vw;
+  height: 12vw;
   margin-top: 0.5vw;
   resize: none;
   border-style: none;
