9ff5e458cfe34a58d0d6447fe1d0a3dfa52f75bc
sangho-jeon <jeonsangho1587@gmail.com>
2022-01-06 13:58:46 +0900

    [Fix] 다른 채팅방에 가던 현상 해결

--- src/components/feature/chatblock/index.jsx
+++ src/components/feature/chatblock/index.jsx
@@ -9,24 +9,24 @@ import { coachInfo } from "../../../utils/coachInfo";
 //   "localhost:3030/chat?roomId=6188e48a9c10dd00179b767f",
 //   { transports: ["websocket"] }
 // );
-const ChatBlock = ({ id, userId }) => {
-  const [currentsocket, setSocket] = useState();
-  //이 부분에서 socket이랑 연결한 후에 prop으로 소켓을 넘겨줘야 리렌더링이 발생하지 않는다.
-  useEffect(() => {
-    async function socketConnection() {
-      const socket = await io("https://api-test.mauve.care/chat", {
-        transports: ["websocket"],
+const ChatBlock = ({ id, userId, currentsocket }) => {
+  // const [currentsocket, setSocket] = useState();
+  // //이 부분에서 socket이랑 연결한 후에 prop으로 소켓을 넘겨줘야 리렌더링이 발생하지 않는다.
+  // useEffect(() => {
+  //   async function socketConnection() {
+  //     const socket = await io("https://api-test.mauve.care/chat", {
+  //       transports: ["websocket"],
 
-        auth: {
-          authorization: `Bearer ${coachInfo.accessToken}`,
-          Refresh: `${coachInfo.refreshToken}`,
-        },
-      });
-      socket.connect();
-      setSocket(socket);
-    }
-    socketConnection();
-  }, []);
+  //       auth: {
+  //         authorization: `Bearer ${coachInfo.accessToken}`,
+  //         Refresh: `${coachInfo.refreshToken}`,
+  //       },
+  //     });
+  //     socket.connect();
+  //     setSocket(socket);
+  //   }
+  //   socketConnection();
+  // }, []);
   return (
     <div>
       {currentsocket ? (
