17f2c8eec749cf6ee0e7670eafd6b9d2c774df21
Hyowon Jeon <hyowon612@gmail.com>
2022-01-27 22:35:44 +0900

    [#129] 채팅버블 디자인 수정

--- src/components/shared/chat/styled.js
+++ src/components/shared/chat/styled.js
@@ -4,44 +4,51 @@ export const BubbleContainer = styled.div`
   display: flex;
   flex-direction: column;
   color: white;
-  margin: 0.15vw 0;
+  margin: 0.5vw 0;
   ${({ sender }) =>
     sender === true
       ? css`
-          align-items: flex-end;
-        `
+        align-items: flex-end;
+      `
       : css`
-          align-items: flex-start;
-        `}
+        align-items: flex-start;
+      `}
 `;
 
 export const Bubble = styled.div`
   max-width: 75%;
   word-break: break-all;
-  padding: 0.5vw 0.8vw;
-  font-size: 1vw;
+  font-size: 1.05vw;
   white-space: pre-wrap;
   color: black;
   ${({ sender }) =>
     sender !== true
       ? css`
-          background-color: #F8F8F8;
-          border-radius: 0.1vw 0.6vw 0.6vw 0.6vw;
-        `
+        background-color: #F8F8F8;
+        border-radius: 0.1vw 0.6vw 0.6vw 0.6vw;
+      `
       : css`
-          background-color: #FFFFFF;
-          border: 0.1vw solid #F0F0F0;
-          border-radius: 0.6vw 0.1vw 0.6vw 0.6vw;
-        `}
+        background-color: #FFFFFF;
+        border: 0.1vw solid #F0F0F0;
+        border-radius: 0.6vw 0.1vw 0.6vw 0.6vw;
+      `}
+  ${({ tag }) =>
+  tag === "chat" || tag === "weight"
+    ? css`
+     padding: 0.5vw 0.8vw;
+    `
+    : css`
+      padding: 0;
+    `}
 `;
 
 export const Image = styled.img`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 12vw;
-  height: 12vw;
-  border-radius: 0.6vw;
+  width: 13vw;
+  height: 13vw;
+  border-radius: 0.5vw;
   object-fit: cover;
 `;
 
