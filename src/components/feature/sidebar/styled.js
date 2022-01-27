0867d985a1d281f49fedbdd49c9481396d049d09
Hyowon Jeon <hyowon612@gmail.com>
2022-01-27 22:36:56 +0900

    [#129] 사이드바 디자인 수정

--- src/components/feature/sidebar/styled.js
+++ src/components/feature/sidebar/styled.js
@@ -6,7 +6,7 @@ export const MainContainer = styled.div`
   align-items: center;
   width: 23vw;
   height: 55vw;
-  background-color: white;
+  background-color: #F2F2F2;
 `;
 
 export const Header = styled.header`
