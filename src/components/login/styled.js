c7d9d998444be59e3d53191e6f6449b0504d279c
Hyowon Jeon <hyowon612@gmail.com>
2022-01-28 11:46:09 +0900

    [#129] 로그인 페이지 디자인 적용

--- src/components/login/styled.js
+++ src/components/login/styled.js
@@ -5,44 +5,59 @@ export const LoginSection = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100vw;
+  height: 55vw;
+  // background-color: #F1F1F1;
 `;
 
 export const Loginblock = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   width: 40vw;
   height: 27vw;
-  margin-top: 5vw;
-  border-radius: 2vw;
-  margin-bottom: 5vw;
-  box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.39);
+  // box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.39);
+`;
+
+export const Logo = styled.img`
+  width: 13vw;
+  height: 5vw;
+  margin-bottom: 0.4vw;
 `;
 
 export const Banner = styled.div`
-  font-size: 4vw;
-  color: #e0b0ff;
-  margin-top: 1.5vw;
-  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 1vw;
+  color: #808080;
+  margin-top: 1.8vw;
+  font-weight: 700;
 `;
 
 export const LoginInput = styled.input`
-  margin-bottom: 5vw;
-  margin-top: 5vw;
-  width: 9vw;
-  height: 2vw;
-  border-color: #d1d1d1;
-  border-radius: 0.5vw;
-  border-style: solid;
+  box-sizing: border-box;
+  margin-bottom: 1.5vw;
+  margin-top: 2.5vw;
+  width: 20vw;
+  height: 4vw;
+  padding: 1.5vw;
+  border-radius: 0.6vw;
+  border: 0.1vw solid #E0E0E0;
+  outline: none;
 `;
 
 export const LoginButton = styled.button`
-  width: 4vw;
-  height: 2vw;
-  margin-bottom: 4vw;
-  background-color: #e0b0ff;
-  border-radius: 5px;
-  border-style: solid;
+  box-sizing: border-box;
+  width: 6vw;
+  height: 3vw;
+  background-color: #3988FF;
+  color: #FFFFFF;
+  padding: 0.3vw;
+  border-radius: 0.2vw;
+  text-align: center;
+  font-size: 1vw;
+  font-weight: bold;
+  border: none;
   cursor: pointer;
 `;
