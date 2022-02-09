5e3c24dcf716923d3f7deafb2c44c4cce2115af8
Hyowon Jeon <hyowon612@gmail.com>
2022-02-09 11:54:29 +0900

    [#142] 캘린더 phase 표시 추가

--- src/components/feature/chatblock/info/profile/calendar/calendar.jsx
+++ src/components/feature/chatblock/info/profile/calendar/calendar.jsx
@@ -2,8 +2,33 @@ import React from "react";
 import './calendar.css';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import moment from 'moment';
 
 const Calendar = (prop) => {
+
+  const phaseEndDate = (record) => {
+    if (record.phase === "delayed") {
+      const today = new Date(moment());
+      return new Date(today).setDate(new Date(today).getDate() + 1);
+    }
+    return new Date(record.end_date).setDate(new Date(record.end_date).getDate() + 1);
+  };
+
+  const phaseColor = (e) => {
+    if (e && e === "period") {
+      return "#F85B39";
+    } else if (e === "golden_time") {
+      return "#FFF172";
+    } else if (e === "effort_time") {
+      return "#D2E7D0";
+    } else if (e === "before_period") {
+      return "#FCD1C6";
+    } else if (e === "delayed") {
+      return "#505050";
+    } else {
+      return;
+    }
+  };
 
 	const isData = (e) => {
     if (JSON.stringify(e) === "[]") {
@@ -24,11 +49,12 @@ const Calendar = (prop) => {
 			end: "",
 		}}          
 		events = {isData(prop.data) &&
-      prop.data.periodRecord.map((period) => (
+      prop.data.periodRecord.map((record) => (
         {
           allDay: true,
-          start: new Date(period.start),
-          end: new Date(period.end).setDate(new Date(period.end).getDate() + 1)
+          start: new Date(record.start_date),
+          end: phaseEndDate(record),
+          color: phaseColor(record.phase),
         }
       ))
 		}
